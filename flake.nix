e1ac5fe2b38863790970cbb865db5fba639027eb
Peter Tonner <peter.tonner@proton.me>
2024-10-06 15:35:53 -0400

    basic working setup

--- flake.nix
+++ flake.nix
@@ -32,14 +32,17 @@
           wasm-bindgen-cli
         ];
         buildInputs = with pkgs; [
+          libGL
           udev
           alsa-lib
           vulkan-loader
+          mesa
           xorg.libX11
           xorg.libXcursor
           xorg.libXi
           xorg.libXrandr # To use the x11 feature
           libxkbcommon
+          wayland # To use the wayland feature
         ];
         webBuildInputs = builtins.concatLists [
           (with pkgs; [
